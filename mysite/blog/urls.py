731119bda38dbbbce84e49cc6e4d2821c8d3aa9f
krody robert <robert@robert-pc.(none)>
2013-04-18 17:24:23 +0300

    fix deletion

---
+++ mysite/blog/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, url
+
+from blog import views
+
+urlpatterns = patterns('',
+	url(r'^$', views.index, name='index'),
+	url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<title_slug>[-\w]+)/$', views.detail, name='detail'),
+	url(r'^register/$', views.registerUser, name='registerUser' ),
+	url(r'^login/$', views.login_user, name='login_user'),
+	url(r'^(?P<name_slug>[-\w]+)/$', views.author, name='author'),
+	
+)
