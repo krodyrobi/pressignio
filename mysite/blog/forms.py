f11beb4c87239ccaba3b2218b6091363c159050c
Dragos <dragos@presslabs.com>
2013-04-19 15:06:14 +0300

    Reupload

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 
+import datetime		
 
 class RegisterForm(forms.Form):
     	username = forms.CharField(max_length=30)
@@ -11,16 +12,6 @@ class RegisterForm(forms.Form):
 
 	email = forms.EmailField()
 
-
-	def clean(self):
-		cleaned_data = self.cleaned_data
-		
-		password1 = cleaned_data.get("password")
-		password2 = cleaned_data.get("pass_check")
-
-
-		if password1 != password2:
-			raise forms.ValidationError("Passwords must be identical.")
-
-		return cleaned_data
-
+class LoginForm(forms.Form):
+	username = forms.CharField(max_length=30)
+	password = forms.CharField(widget=forms.PasswordInput)
