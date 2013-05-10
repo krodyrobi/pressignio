5245f06e798cd2980ae62521ca8592ccc5da5830
krody robert <robert@robert-pc.(none)>
2013-05-10 14:13:49 +0300

    Finished tranlslations

--- mysite/static/bootstrap/js/patch.js
+++ mysite/static/bootstrap/js/patch.js
@@ -1,6 +1,5 @@
-
 $(document).ready(function(){
-	$("#language_selector").click(function() {
+	$("#language_selector*").click(function() {
 	$('#language_form :input[name="language"]').val($(this).attr("data-language-code"));
 	$('#language_form').submit();
 	});
